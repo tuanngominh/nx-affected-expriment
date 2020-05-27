4936228a3b8388e944a84de51ba56c40baa36a55
Tuan Ngo <tuannm79@gmail.com>
2020-05-27 14:04:17 +0700

    deploy

--- libs/functions/feature-1/src/lib/functions-feature-1.ts
+++ libs/functions/feature-1/src/lib/functions-feature-1.ts
@@ -1,10 +1,14 @@
 import * as functions from 'firebase-functions';
 import { CallableContext } from 'firebase-functions/lib/providers/https';
 
+export function f1() {
+  return {message: 'feature 1 done.'};
+}
+
 export const feature1 = {
   feature1: functions.https.onCall(
     (data: any, context: CallableContext) => {
-      return {message: 'feature 1 done'};
+      return f1();
     }
   ),
 }
