d14c93bd7ac1f55d1b85a2347cf0228cac14f21a
Tuan Ngo <tuannm79@gmail.com>
2020-05-27 22:58:55 +0700

    fix previous tag / current tag

--- libs/functions/feature-2/src/lib/functions-feature-2.ts
+++ libs/functions/feature-2/src/lib/functions-feature-2.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import { CallableContext } from 'firebase-functions/lib/providers/https';
 
 export function f2() {
-  return {message: 'feature 2 done..'};
+  return {message: 'feature 2 done...'};
 }
 
 export const feature2 = {
