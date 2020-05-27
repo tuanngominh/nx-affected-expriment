f76fee830f3f00693ea1ff3a969451990d033f53
Tuan Ngo <tuannm79@gmail.com>
2020-05-27 23:02:12 +0700

    fix previous tag / current tag

--- libs/functions/feature-2/src/lib/functions-feature-2.spec.ts
+++ libs/functions/feature-2/src/lib/functions-feature-2.spec.ts
@@ -2,6 +2,6 @@ import { f2 } from './functions-feature-2';
 
 describe('functionsFeature2', () => {
   it('should work', () => {
-    expect(f2()).toEqual({message: 'feature 2 done..'});
+    expect(f2()).toEqual({message: 'feature 2 done...'});
   });
 });
